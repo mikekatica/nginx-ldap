9fb5524464867d9a451de58db6ec876470f8a961
mkatica <michael.katica@videa.tv>
2017-05-31 15:29:34 -0400

    Added S3 download

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@
 set -e
 if test -e /nginx.conf; then
     cp -v /nginx.conf /etc/nginx/nginx.conf
+else
+    curl -O /etc/nginx/nginx.conf {$S3_LOCATION}
 fi
 exec dockerize -stdout /var/log/nginx/access.log -stderr /var/log/nginx/error.log /usr/sbin/nginx -c /etc/nginx/nginx.conf -g "daemon off;"
